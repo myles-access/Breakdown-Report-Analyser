f472d2e0fd2fd9fbbaaea0a401c07df404e0c912
myles <myles@accesselevators.com.au>
2021-03-31 16:27:23 +1100

    initial design and import of excel objects
    added buttons
    added excel functionality 
    added file picker dialogue

--- Monthly Breakdown Analyser/Monthly Breakdown Analyser/Form1.Designer.vb
+++ Monthly Breakdown Analyser/Monthly Breakdown Analyser/Form1.Designer.vb
@@ -22,10 +22,70 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.runButton = New System.Windows.Forms.Button()
+        Me.fileSelectButton = New System.Windows.Forms.Button()
+        Me.fileLabel = New System.Windows.Forms.Label()
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'runButton
+        '
+        Me.runButton.Font = New System.Drawing.Font("Calibri", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.runButton.Location = New System.Drawing.Point(12, 13)
+        Me.runButton.Name = "runButton"
+        Me.runButton.Size = New System.Drawing.Size(454, 175)
+        Me.runButton.TabIndex = 0
+        Me.runButton.Text = "Generate Report Analysis"
+        Me.runButton.UseVisualStyleBackColor = True
+        '
+        'fileSelectButton
+        '
+        Me.fileSelectButton.BackgroundImage = CType(resources.GetObject("fileSelectButton.BackgroundImage"), System.Drawing.Image)
+        Me.fileSelectButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.fileSelectButton.Location = New System.Drawing.Point(12, 234)
+        Me.fileSelectButton.Name = "fileSelectButton"
+        Me.fileSelectButton.Size = New System.Drawing.Size(126, 130)
+        Me.fileSelectButton.TabIndex = 1
+        Me.fileSelectButton.UseVisualStyleBackColor = True
+        '
+        'fileLabel
+        '
+        Me.fileLabel.AutoSize = True
+        Me.fileLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.fileLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.fileLabel.Location = New System.Drawing.Point(144, 288)
+        Me.fileLabel.Name = "fileLabel"
+        Me.fileLabel.Size = New System.Drawing.Size(126, 24)
+        Me.fileLabel.TabIndex = 2
+        Me.fileLabel.Text = "No file selected"
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 22.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(478, 376)
+        Me.Controls.Add(Me.fileLabel)
+        Me.Controls.Add(Me.fileSelectButton)
+        Me.Controls.Add(Me.runButton)
+        Me.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Breakdown Report Analyser"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents runButton As Button
+    Friend WithEvents fileSelectButton As Button
+    Friend WithEvents fileLabel As Label
+    Friend WithEvents OpenFileDialog As OpenFileDialog
 End Class
--- Monthly Breakdown Analyser/Monthly Breakdown Analyser/Form1.vb
+++ Monthly Breakdown Analyser/Monthly Breakdown Analyser/Form1.vb
@@ -1,3 +1,50 @@
-﻿Public Class Form1
+﻿Imports Microsoft.Office.Interop
 
+Public Class Form1
+    Dim fileSelected As Boolean
+
+    Private Sub RunButton_Click(sender As Object, e As EventArgs) Handles runButton.Click
+        Dim run1Row As Integer
+        Dim run2Row As Integer
+        Dim run3Row As Integer
+        Dim run4Row As Integer
+
+        'set button colour to light green to show the user has clicked and the program is working
+        runButton.BackColor = Color.LightSkyBlue
+        If fileSelected = True Then
+            runButton.BackColor = Color.LightGreen
+        Else
+            runButton.BackColor = Color.Red
+        End If
+
+    End Sub
+
+    Private Sub FileSelectButton_Click(sender As Object, e As EventArgs) Handles fileSelectButton.Click
+        'set button colour to light blue to show the user has clicked and the program is running
+        fileSelectButton.BackColor = Color.LightSkyBlue
+
+        'setup windows file picker to show users downloads folder & only .xlxs files to prevent errors
+        OpenFileDialog.Title = "Select the monthly report"
+        Dim strUser As String
+        strUser = Environ("username")
+        OpenFileDialog.InitialDirectory = "C:\Users\" & strUser & "\downloads"
+        ' OpenFileDialog.Filter = "Excel File|*.xlxs"
+        OpenFileDialog.Multiselect = False
+
+        'open the file picker, set label to the selected file, & set the button to green to show the success of the process
+        If OpenFileDialog.ShowDialog = Windows.Forms.DialogResult.Cancel Then
+            fileLabel.Text = "File selection unsucessful"
+            fileSelected = False
+            fileSelectButton.BackColor = Color.Red
+        Else
+            fileLabel.Text = ("File Retrived Sucessfully" & vbCrLf & OpenFileDialog.FileName)
+            fileSelected = True
+            fileSelectButton.BackColor = Color.LightGreen
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
