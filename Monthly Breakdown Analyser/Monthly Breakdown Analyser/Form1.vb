340fc421149c7e2b27c649ff3db344a534249790
myles <myles@accesselevators.com.au>
2021-04-07 09:07:16 +1000

    Excel loops added, error with arrays
    started the excel loops, error on writing to number of jobs per address array
    successfully opens and reads excel file 
    appears to successfully write to address array

--- Monthly Breakdown Analyser/Monthly Breakdown Analyser/Form1.vb
+++ Monthly Breakdown Analyser/Monthly Breakdown Analyser/Form1.vb
@@ -1,17 +1,197 @@
-﻿Imports Microsoft.Office.Interop
+﻿Imports Excel = Microsoft.Office.Interop.Excel
 
 Public Class Form1
     Dim fileSelected As Boolean
+    Dim xlApp As Excel.Application
+    Dim xlWorkBook As Excel.Workbook
+    Dim xlWorkSheet As Excel.Worksheet
+    Dim int As Integer
+    Dim loopCheker As Boolean = False
+    Dim loopChecker2 As Boolean = False
+    Dim run1Row As Integer = 1
+    Dim run1Cell As String
+    Dim run1Addresses() As String
+    Dim run1numbers() As Integer
+    Dim run2Row As Integer = 1
+    Dim run2Cell As String
+    Dim run2Addresses() As String
+    Dim run2numbers() As Integer
+    Dim run3Row As Integer = 1
+    Dim run3Cell As String
+    Dim run3Addresses() As String
+    Dim run3numbers() As Integer
+    Dim run4Row As Integer = 1
+    Dim run4Cell As String
+    Dim run4Addresses() As String
+    Dim run4numbers() As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 
     Private Sub RunButton_Click(sender As Object, e As EventArgs) Handles runButton.Click
-        Dim run1Row As Integer
-        Dim run2Row As Integer
-        Dim run3Row As Integer
-        Dim run4Row As Integer
 
         'set button colour to light green to show the user has clicked and the program is working
         runButton.BackColor = Color.LightSkyBlue
         If fileSelected = True Then
+
+            xlApp = New Excel.Application
+            xlWorkBook = xlApp.Workbooks.Open(OpenFileDialog.FileName)
+            xlWorkSheet = xlWorkBook.Worksheets("Sheet")
+
+            ResetVarsForLoops()
+            Do While loopCheker = False
+                run1Cell = ("A" & run1Row)
+
+                If xlWorkSheet.Range(run1Cell).Value = "Run 1" Then
+                    loopCheker = True
+                ElseIf run1Row >= 1000 Then
+                    loopCheker = True
+                End If
+                run1Row += 1
+            Loop
+            ResetVarsForLoops()
+            Do While loopCheker = False
+                run2Cell = ("A" & run2Row)
+
+                If xlWorkSheet.Range(run2Cell).Value = "Run 2" Then
+                    loopCheker = True
+                ElseIf run2Row >= 1000 Then
+                    loopCheker = True
+                End If
+                run2Row += 1
+            Loop
+            ResetVarsForLoops()
+            Do While loopCheker = False
+                run3Cell = ("A" & run3Row)
+
+                If xlWorkSheet.Range(run3Cell).Value = "Run 3" Then
+                    loopCheker = True
+                ElseIf run3Row >= 1000 Then
+                    loopCheker = True
+                End If
+                run3Row += 1
+            Loop
+            ResetVarsForLoops()
+            Do While loopCheker = False
+                run4Cell = ("A" & run4Row)
+
+                If xlWorkSheet.Range(run4Cell).Value = "Run 4" Then
+                    loopCheker = True
+                ElseIf run4Row >= 1000 Then
+                    loopCheker = True
+                End If
+                run4Row += 1
+            Loop
+
+
+
+            ResetVarsForLoops()
+            Do While loopCheker = False
+                Dim cell As String
+                Dim i As Integer = 0
+
+                cell = ("B" & run1Row + i)
+                If run1Row + i >= run2Row Then
+                    loopCheker = True
+                ElseIf xlWorkSheet.Range(cell).Value <> "" Then
+                    Dim i2 As Integer = 0
+                    Dim cell2 As String
+
+                    run1Addresses(i) = xlWorkSheet.Range(cell).Value
+                    Do While loopCheker = False
+                        cell2 = ("C" & run2Row + i2)
+                        If xlWorkSheet.Range(cell2).Value <> "" Then
+                            run1numbers(i) += 1
+                        ElseIf i2 >= run2Row Then
+                            loopChecker2 = True
+                        End If
+                        i2 += 1
+                    Loop
+                End If
+                i += 1
+            Loop
+            ResetVarsForLoops()
+            Do While loopCheker = False
+                Dim cell As String
+                Dim i As Integer = 0
+
+                cell = ("B" & run2Row + i)
+                If run2Row + i >= run3Row Then
+                    loopCheker = True
+                ElseIf xlWorkSheet.Range(cell).Value <> "" Then
+                    Dim i2 As Integer = 0
+                    Dim cell2 As String
+
+                    run2Addresses(i) = xlWorkSheet.Range(cell).Value
+                    Do While loopChecker2 = False
+                        cell2 = ("C" & run2Row + i2)
+                        If xlWorkSheet.Range(cell2).Value <> "" Then
+                            run2numbers(i) += 1
+                        ElseIf i2 >= run3Row Then
+                            loopChecker2 = True
+                        End If
+                        i2 += 1
+                    Loop
+                End If
+                i += 1
+            Loop
+            ResetVarsForLoops()
+            Do While loopCheker = False
+                Dim cell As String
+                Dim i As Integer = 0
+
+                cell = ("B" & run3Row + i)
+                If run3Row + i >= run4Row Then
+                    loopCheker = True
+                ElseIf xlWorkSheet.Range(cell).Value <> "" Then
+                    Dim i2 As Integer = 0
+                    Dim cell2 As String
+
+                    run3Addresses(i) = xlWorkSheet.Range(cell).Value
+                    Do While loopChecker2 = False
+                        cell2 = ("C" & run3Row + i2)
+                        If xlWorkSheet.Range(cell2).Value <> "" Then
+                            run3numbers(i) += 1
+                        ElseIf i2 >= run4Row Then
+                            loopChecker2 = True
+                        End If
+                        i2 += 1
+                    Loop
+                End If
+                i += 1
+            Loop
+            ResetVarsForLoops()
+            Do While loopCheker = False
+                Dim cell As String
+                Dim i As Integer = 0
+
+                cell = ("B" & run4Row + i)
+                If run4Row + i >= 1000 Then
+                    loopCheker = True
+                ElseIf xlWorkSheet.Range(cell).Value <> "" Then
+                    Dim i2 As Integer = 0
+                    Dim cell2 As String
+
+                    run4Addresses(i) = xlWorkSheet.Range(cell).Value
+                    Do While loopChecker2 = False
+                        cell2 = ("C" & run2Row + i2)
+                        If xlWorkSheet.Range(cell2).Value <> "" Then
+                            run4numbers(i) += 1
+                        ElseIf i2 >= 1000 Then
+                            loopChecker2 = True
+                        End If
+                        i2 += 1
+                    Loop
+                End If
+            Loop
+
+            xlWorkBook.Close()
+            xlApp.Quit()
+            releaseObject(xlApp)
+            releaseObject(xlWorkBook)
+            releaseObject(xlWorkSheet)
+
             runButton.BackColor = Color.LightGreen
         Else
             runButton.BackColor = Color.Red
@@ -44,7 +224,26 @@ Public Class Form1
 
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        xlWorkBook.Close()
+        xlApp.Quit()
+    End Sub
+
+    Private Sub ResetVarsForLoops()
+        loopCheker = False
+        loopChecker2 = False
+        int = -1
+    End Sub
+
 End Class
