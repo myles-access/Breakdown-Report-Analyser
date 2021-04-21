5550c1183fe81ba0c5e2683acf23f0f21c6199da
myles <myles@accesselevators.com.au>
2021-04-21 16:27:42 +1000

    fixed code comments
    added comments where some were missing
    restructured code for ease of use and debugging
    removed some unused code from previous debugging

--- Monthly Breakdown Analyser/Monthly Breakdown Analyser/Form1.vb
+++ Monthly Breakdown Analyser/Monthly Breakdown Analyser/Form1.vb
@@ -6,7 +6,6 @@ Public Class Form1
     Dim xlWorkBook As Excel.Workbook
     Dim xlWorkSheet As Excel.Worksheet
     Dim loopCheker As Boolean = False
-    Dim loopChecker2 As Boolean = False
     Dim run2Row As Integer = 1
     Dim run3Row As Integer = 1
     Dim run4Row As Integer = 1
@@ -23,7 +22,6 @@ Public Class Form1
     Dim addressCounter As Integer = 0
     Dim counter As Integer = 0
     Dim firstLoop As Boolean = True
-    Dim cellRun As String
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -35,10 +33,12 @@ Public Class Form1
         runButton.BackColor = Color.LightSkyBlue
         If fileSelected = True Then
 
+            'asign the excel vars their values of the excel application
             xlApp = New Excel.Application
             xlWorkBook = xlApp.Workbooks.Open(OpenFileDialog.FileName)
             xlWorkSheet = xlWorkBook.Worksheets("Sheet")
 
+            'reset the vars used for loop tracking then find the row containing RUN 2
             ResetVarsForLoops()
             run2Row = 0
             Do While loopCheker = False
@@ -55,6 +55,7 @@ Public Class Form1
                 End If
             Loop
 
+            'reset the vars used for loop tracking then find the row containing RUN 3
             ResetVarsForLoops()
             run3Row = 0
             Do While loopCheker = False
@@ -71,6 +72,7 @@ Public Class Form1
                 End If
             Loop
 
+            'reset the vars used for loop tracking then find the row containing RUN 4
             ResetVarsForLoops()
             run4Row = 0
             Do While loopCheker = False
@@ -87,6 +89,7 @@ Public Class Form1
                 End If
             Loop
 
+            'reset the vars used for loop tracking then find the row containing the copyright notice used to find the end of the page
             ResetVarsForLoops()
             endRow = 0
             Do While loopCheker = False
@@ -105,12 +108,16 @@ Public Class Form1
 
             ResetVarsForLoops()
             Do Until loopCheker = True
+                'update the cell var to be the next cell down then check if it is a new address or if it is a job under the previous address
                 cellAddress = ("B" & run2Row + addressCounter)
 
+                'if the loop has found the row containg the next run it has gone too far and will exit the loop
                 If xlWorkSheet.Range(cellAddress).Row >= run3Row Then
                     loopCheker = True
 
+                    'if the checked cell contains text it is a new address and will add it to the list
                 ElseIf String.IsNullOrEmpty(xlWorkSheet.Range(cellAddress).Value) = False Then
+                    'providing that this isnt the first address found in the loop the finding of a new address will mean the end of the old one and the program will write the amount of jobs found under the previous address to the list
                     If firstLoop = False Then
                         run2numbers.Add(counter)
 
@@ -120,8 +127,11 @@ Public Class Form1
                     run2Addresses.Add(xlWorkSheet.Range(cellAddress).Value)
 
                 ElseIf String.IsNullOrEmpty(xlWorkSheet.Range(cellAddress).Value) Then
-                    counter += 1
+                    'increments every time a job is found starting at -1 moving to 0 to accomidate the blank rows under each address
+                    If firstLoop = False Then
+                        counter += 1
 
+                    End If
                 End If
                 addressCounter += 1
 
@@ -144,8 +154,10 @@ Public Class Form1
                     run3Addresses.Add(xlWorkSheet.Range(cellAddress).Value)
 
                 ElseIf String.IsNullOrEmpty(xlWorkSheet.Range(cellAddress).Value) Then
-                    counter += 1
+                    If firstLoop = False Then
+                        counter += 1
 
+                    End If
                 End If
                 addressCounter += 1
 
@@ -168,22 +180,27 @@ Public Class Form1
                     run4Addresses.Add(xlWorkSheet.Range(cellAddress).Value)
 
                 ElseIf String.IsNullOrEmpty(xlWorkSheet.Range(cellAddress).Value) Then
-                    counter += 1
+                    If firstLoop = False Then
+                        counter += 1
 
+                    End If
                 End If
                 addressCounter += 1
 
             Loop
 
+            'cleanup by closign the excel application and sending all the use memory to garbage collection 
             xlWorkBook.Close()
             xlApp.Quit()
-            releaseObject(xlApp)
-            releaseObject(xlWorkBook)
-            releaseObject(xlWorkSheet)
+            ReleaseObject(xlApp)
+            ReleaseObject(xlWorkBook)
+            ReleaseObject(xlWorkSheet)
 
+            'change button to green to indicate success
             runButton.BackColor = Color.LightGreen
 
         Else
+            'change button to red to show a file wasnt selected 
             runButton.BackColor = Color.Red
 
         End If
@@ -218,7 +235,7 @@ Public Class Form1
 
     End Sub
 
-    Private Sub releaseObject(ByVal obj As Object)
+    Private Sub ReleaseObject(ByVal obj As Object)
         Try
             System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
             obj = Nothing
@@ -230,6 +247,10 @@ Public Class Form1
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' magical testing button land
+        'everything here can be removed or commented out as needed
+
+
         ' xlWorkBook.Close()
         'xlApp.Quit()
         MsgBox(" Run 2 lenghths " & run2Addresses.Count & " & " & run2numbers.Count &
@@ -238,30 +259,22 @@ Public Class Form1
         MsgBox(" R2 " & run2Row &
                " R3 " & run3Row &
                " R4 " & run4Row)
-
-        'Dim s As String
-        's = ""
-        'For i = 1 To run2Addresses.Length - 1
-        '    s = s & run2Addresses(i) & " "
-        'Next
-        'MsgBox(s)
-        's = ""
-        'For i = 1 To run2numbers.Length - 1
-        '    s = s & run2numbers(i) & " "
-        'Next
-        'MsgBox(s)
+        Dim test As String
+        For Each Str As String In run2Addresses
+            test = test & " " & vbCrLf & Str
+        Next
+        MsgBox(test)
 
     End Sub
 
     Private Sub ResetVarsForLoops()
-        'MsgBox("loop done " & i & " times")
-        loopCheker = False
-        loopChecker2 = False
-        cellRun = ""
-        cellAddress = ""
-        addressCounter = 1
-        counter = -1
-        firstLoop = True
+        'all the vars that need to be reset for loops
+
+        loopCheker = False  'bool - checks if the loop is finished
+        cellAddress = ""    'string - holds the range of the cell being checked
+        addressCounter = 0  'int - counts the number of addresses found
+        counter = -1        'int - counts the number of jobs found
+        firstLoop = True    'bool - checks if this is the first itteration of the loop
     End Sub
 
 End Class
